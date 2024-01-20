67f605a6890afc926f14c611ce67d5a48caa6e0e
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-01-20 17:32:05 +0000

    Kata 5 terminada

--- src/main/java/kata5/Main.java
+++ src/main/java/kata5/Main.java
@@ -1,6 +1,12 @@
 package kata5;
 
+import spark.Spark;
+
 public class Main {
     public static void main(String[] args) {
+        CommandExecutor.put("largestPrime", new LargestPrimeCommand());
+        Spark.port(8080);
+        Spark.get("/largestPrime", ((request, response) ->
+                CommandExecutor.with(request, response).execute("largestPrime")));
     }
 }
